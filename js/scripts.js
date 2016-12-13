c6cfae3def58986627db0f73f4b532bac4e2a537
LiamRosvel <adrianrosase@gmail.com>
2016-12-13 12:36:54 -0700

    Se termino home de npm, se agrego al final la funcion JS para mostrar el menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
+'use strict';
+
 //funcion para mostrar el menu off-canvas
-(function(){
+(function () {
     //casheamos los selectores para mejorar el rendimiento
     var button = document.getElementById('button');
     var menu = document.getElementById('main-menu');
 
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function () {
         menu.classList.toggle('mostrar');
     });
 })();
